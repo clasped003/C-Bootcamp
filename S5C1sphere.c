fc1d6d635e523825bc1470a1ec43b301160ffd07
clasped003 <clasped003@gmail.com>
2022-01-12 12:46:49 -0800

    Began next challenge, verified startup ok, copied goals and set main function.

---
+++ S5C1sphere.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Sphere Challange:
+ *
+ * Task: Calculate Surface and Volume of a sphere.
+ *
+ *  Steps:
+ *
+ *  -Prompt user to enter the radius.
+ *  -Calculate the surface area
+ *  -Calculate the enclosed volume
+ *  -Print the radius and the results in decimal format(w/2 digit precision)
+ *  -Print the radius & results in sci-notation(w/3 digit precision)
+ *  -Check the results
+ *  */
+
+int main(void){
+    
+    puts("Sphere Calculator!");
+    puts("Please enter the values for the volume and surface.");
+
+    
+    return 0;
+}
